6d707b9545944d76ffa05dc018ca16f18dff4af0
MrNoone <mr.noone2030@gmail.com>
2022-03-25 10:15:51 +0530

    Initial lient changes

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  users: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').pipe()
+    .subscribe(data => {this.users = data},
+    error => console.log(error),
+    () => console.log('completed')
+    );
+  }
+}
